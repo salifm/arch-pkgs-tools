b8179427757ca11b43b94d3a121891562ffafba5
Salif Mehmed <mail@salif.eu>
2021-01-28 00:42:09 +0200

    fix init.bash

--- src/init.bash
+++ src/init.bash
@@ -6,8 +6,8 @@ function ap_main {
 	local AP_DOCKER_BUILD_DIR=${AP_DOCKER_BUILD_DIR:-"/tmp/docker-build"}
 
 	mkdir -p "$AP_DOCKER_BUILD_DIR"
-	cp /usr/share/arch-pkgs/ap-tools/Dockerfile "$AP_DOCKER_BUILD_DIR"/
-	cp /usr/share/arch-pkgs/ap-tools/build "$AP_DOCKER_BUILD_DIR"/
+	cp /usr/share/ap-tools/Dockerfile "$AP_DOCKER_BUILD_DIR"/
+	cp /usr/share/ap-tools/build "$AP_DOCKER_BUILD_DIR"/
 	cp /etc/ap-tools.conf "$AP_DOCKER_BUILD_DIR"/
 	docker build -t arch-pkgs "$AP_DOCKER_BUILD_DIR"/
 	gio trash "$AP_DOCKER_BUILD_DIR"
